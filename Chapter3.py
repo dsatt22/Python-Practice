6cab52f512264b3ebce7faca8f5cd31532620501
Dustin Sattler <dustin.r.sattler@gmail.com>
2020-08-05 22:19:04 -0400

    Initial commit

---
+++ Chapter3.py
@@ -0,0 +1,82 @@
+#** LISTS **
+
+#Practice
+
+#names = ['caila', 'jared', 'aaron', 'krystal', 'josh']
+#print(names[0])
+#print(names[1])
+#print(names[2])
+#print(names[3])
+#print(names[4])
+
+#print(f"{names[0].title()} is my girlfriend!")
+#print(f"{names[1].title()} is my friend!")
+#print(f"{names[2].title()} is my friend!")
+#print(f"{names[3].title()} is my friend!")
+#print(f"{names[4].title()} is my friend!")
+
+#carBrands = ['bmw', 'audi', 'mercedes', 'tesla']
+#print(f"I really want to own a {carBrands[2].title()} & a {carBrands[3].title()}")
+
+#print(carBrands)
+
+#popped_carBrands = carBrands.pop()
+#print(carBrands)
+#print(popped_carBrands)
+
+#too_expensive = 'mercedes'
+#carBrands.remove(too_expensive)
+#print(carBrands)
+#print(f"A {too_expensive.title()} is too expensive for me.")
+
+#Practice #2
+
+#dinnerGuests = ['caila', 'jared', 'aaron', 'brandon']
+#print(dinnerGuests)
+#print(f"{dinnerGuests[0].title()}, I would like to invite you to a special dinner party, in honor of me!")
+#print(f"{dinnerGuests[1].title()}, I would like to invite you to a special dinner party, in honor of me!")
+#print(f"{dinnerGuests[2].title()}, I would like to invite you to a special dinner party, in honor of me!")
+#print(f"{dinnerGuests[3].title()}, I would like to invite you to a special dinner party, in honor of me!")
+
+#print(f"{dinnerGuests[2].title()} cannot make it.")
+#dinnerGuests.remove('aaron')
+#print(dinnerGuests)
+#dinnerGuests.insert(2,'joey')
+#print(f"{dinnerGuests[0].title()} you are invited to my dinner party.")
+#print(f"{dinnerGuests[1].title()} you are invited to my dinner party.")
+#print(f"{dinnerGuests[2].title()} you are invited to my dinner party.")
+#print(f"{dinnerGuests[3].title()} you are invited to my dinner party.")
+
+#dinnerGuests.sort(reverse=True)
+#print(dinnerGuests)
+
+#carBrands = ['audi', 'mercedes', 'bmw', 'tesla', 'chevy']
+#print("Here is the original list:")
+#print(carBrands)
+
+#print("Here is the sorted list:")
+#print(sorted(carBrands))
+
+#print("Here is the original list again:")
+#print(carBrands)
+
+#carBrands.reverse()
+#print(carBrands)
+
+#print(len(carBrands))
+
+#places = ['italy', 'greece', 'australia', 'new zealand', 'turks & caicos']
+#print(places)
+#print(sorted(places))
+#print(places)
+#print(sorted(places, reverse=True))
+#print(places)
+#places.reverse()
+#print(places)
+#places.reverse()
+#print(places)
+#places.sort()
+#print(places)
+#places.sort(reverse=True)
+#print(places)
+
---
+++ main.py
@@ -0,0 +1,65 @@
+# print("Hello World")
+#**Chapter 2 Variables & Simple Data Types**
+#num1 = input("Enter First number: ")
+#num2 = input("Enter Second number: ")
+#sum = float(num1) + float(num2)
+#print(sum)
+
+#if 5==5:
+#    print("You successfully learned if statement.")
+
+#if 5==6:
+ #   print("You successfully learned if statement.")
+#else:
+ #   print("Oops! not this time.")
+
+#if 5==4
+#   print("An if statement. Oh!")
+#elif 4==4:
+#    print("That's something new!")
+#elif 4>=9:
+#    print("Really?")
+#else:
+#    print("Not this time")
+
+#for num in range(1,101):
+#    print(num)
+
+#for num in range(2,10,2):
+#    print(num)
+
+#num = 0
+#while(num<10):
+#    print(num)
+#    num = num + 1
+
+#def helloWorld():
+#    print("Hello World")
+#helloWorld()
+
+#def addNumbers(num1, num2):
+#    sum = num1 + num2
+#    print(sum)
+#    return
+#addNumbers(1,2)
+#addNumbers(5,8)
+
+#f-strings (f=format)#
+
+#first_name = "dustin"
+#last_name = "sattler"
+#full_name = f"{first_name} {last_name}"
+#print(f"Hello, {full_name.title()}!")
+
+#first_name = "caila"
+#last_name = "anderson"
+#full_name = f"{first_name} {last_name}"
+#message = f"Hello, {full_name.title()}!"
+#print(message)
+
+#favorite_number = 12
+#message = f"Your favorite number is {favorite_number}"
+#print(message)
+
+
+
