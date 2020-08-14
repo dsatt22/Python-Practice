10c38d4c28e9e49895c251411a3f9160873a08ae
Dustin Sattler <dustin.r.sattler@gmail.com>
2020-08-14 15:51:38 -0400

    Updated Chapter7.py
    Installed pylint on my laptop to run the code in terminal.
    
    Added to chapter7.py with code for while loops.

--- Chapter7.py
+++ Chapter7.py
@@ -93,4 +93,24 @@
 #    print(f"\nThe number you entered is NOT a multiple of 10.")
 
 
-#-------------------------------
+#------------------------------- Introducing While Loops ---------------------------------------------------
+
+## While Loop in Action ##
+#current_number =  1
+#while current_number <= 5:
+#    print(current_number)
+#    current_number += 1
+## The += operator is shorthand for current_number = current_number + 1
+
+
+## Letting the User Choose When to Quit ##
+prompt = "\nTell me something, and I will repeat it back to you: "
+prompt += "\nEnter 'quit' to end the program. "
+message = ""
+while message != 'quit': 
+    message = input(prompt)
+#    print(message)
+
+## We don't want Python to print 'quit' once it is entered, so here is a quick fix for that
+    if message != 'quit':
+        print(message)
